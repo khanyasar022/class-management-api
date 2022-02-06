12ea23d73cb6c0ec000755e93b479bdba3b4cd9d
khanyasar022 <38328077+khanyasar022@users.noreply.github.comyasarfarfat786@gmail.com>
2022-02-06 15:58:19 +0530

    fixed spelling errors

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,5 +1,5 @@
-const mogoose = require('mongoose')
-mogoose
+const mongoose = require('mongoose')
+mongoose
   .connect(process.env.DATABASE)
   .then(() => {
     console.log("database connected")
